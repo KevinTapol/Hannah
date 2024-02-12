dc26bf7a638d5f94e584cb215c8eb6eb07f4f2f3
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-02-11 19:40:58 -0600

    fixed css classes typo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,13 +7,16 @@ const Main = () => {
   return (
     <div>
         <div className='flex justify-around'>
-          <img className='max-w-[200px]' src={Hannah} alt="" />
-          <img className='max-w-[200px]' src={Logo} alt="" />
+          <img className='max-w-[100px]' src={Hannah} alt="" />
+          <img className='max-w-[100px]' src={Logo} alt="" />
         </div>
 
-        <img className='block mx-auto max-2-[100px]' src={Product} alt="" />
+        <div className='flex items-center'>
+            <img className='max-w-[200px]' src={Product} alt="" />
+        </div>
         
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col
+         items-center'>
           <h5 className='text-white text-sm'>Allergen friendly options are available.</h5>
           <h5 className='text-white text-sm'>Prices will vary for special requests.</h5>
           <h5 className='text-white text-sm'>Delivery fees not included and vary on distance.</h5>
