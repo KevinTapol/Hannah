2b70aa37810dac6924a4925ab2205916a667cc29
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-02-16 18:42:59 -0600

    fixing footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main from './components/Main'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className='bg-black'>
       <Main />
+      {/* <Footer /> */}
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-col justify-center items-center bg-black'>
+        <div className=''>
+          <a target='_blank' href="https://kevintapolcsanyi.netlify.app/" className='flex text-gray-400 scale-[80%] hover:scale-[100%] ease-in duration-200 cursor-pointer'><img className='scale-[60%]' src={favicon} alt="" /><h3 className='m-auto text-lg' >  &copy; 2023 Kevin Tapolcsanyi</h3></a>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Footer
